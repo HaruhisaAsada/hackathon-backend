0fe0ad883e6525ec140bb0e106e2f6863b4e8a6b
HaruhisaAsada <uttdjasd12@g.ecc.u-tokyo.ac.jp>
2025-12-09 21:04:26 +0900

    "added cors system"

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from routers import user #ここにリソースを追加
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
