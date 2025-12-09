05eba44d6570ba2723652f2219ff641b64a8ec0c
HaruhisaAsada <uttdjasd12@g.ecc.u-tokyo.ac.jp>
2025-12-09 18:41:05 +0900

    fixed again

--- db.py
+++ db.py
@@ -8,14 +8,22 @@ load_dotenv()
 
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+ENV = os.getenv("ENV")
 
-DATABASE_URL = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
-    f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-)
+if ENV == "local":
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = os.getenv("MYSQL_PORT")
+    DATABASE_URL = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+    )
+elif ENV == "cloud":
+    INSTANCE_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
+    DATABASE_URL = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@/{MYSQL_DATABASE}?unix_socket=/cloudsql/{INSTANCE_NAME}"
+    )
 
 engine = create_engine(
     DATABASE_URL,
