d075136474d03dc1b84cd86594613ba15bce08a4
HaruhisaAsada <uttdjasd12@g.ecc.u-tokyo.ac.jp>
2025-12-13 23:44:08 +0900

    added environment variable

--- gcs_utils.py
+++ gcs_utils.py
@@ -8,9 +8,6 @@ from schemas.items import UploadUrlRequest
 from google.cloud import storage
 
 
-BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
-
-
 def get_storage_client() -> storage.Client:
     return storage.Client()
 
@@ -18,14 +15,12 @@ def get_storage_client() -> storage.Client:
 def generate_upload_signed_url(filename: str, content_type: str | None = None) -> tuple[str, str]:
     if not filename.lower().endswith((".jpg", ".jpeg")):
         raise HTTPException(status_code=400, detail="JPEG画像のみアップロード可能です")
-    if BUCKET_NAME is None:
-        raise RuntimeError("環境変数 GCS_BUCKET_NAME が設定されていません")
 
     client = get_storage_client()
-    bucket = client.bucket(BUCKET_NAME)
+    bucket = client.bucket("fleamarketapp")
 
     # ファイル名から拡張子を取得しておく（image.jpg など）
-    ext = os.path.splitext(filename)[1]  # ".jpg" など
+    ext = os.path.splitext(filename)[1]
     # GCS 上のオブジェクト名（重複防止に UUID を付ける）
     object_name = f"items/{uuid.uuid4()}{ext}"
 
@@ -42,9 +37,10 @@ def generate_upload_signed_url(filename: str, content_type: str | None = None) -
         expiration=timedelta(minutes=5),
         method="PUT",
         content_type=content_type,
+        service_account_email=os.getenv("GCP_SERVICE_ACCOUNT_EMAIL"),
     )
 
     # ブラウザからアクセスするときに使う URL（公開バケットならそのままアクセス可）
-    public_url = f"https://storage.googleapis.com/{BUCKET_NAME}/{object_name}"
+    public_url = f"https://storage.googleapis.com/fleamarketapp/{object_name}"
 
     return upload_url, public_url
