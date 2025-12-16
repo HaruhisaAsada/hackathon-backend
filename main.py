28dbe8be7966982021b207cb0f2ac2c8772f46bb
HaruhisaAsada <uttdjasd12@g.ecc.u-tokyo.ac.jp>
2025-12-16 17:31:53 +0900

    購入ルーターを追加し、CORSのオリジンURLを更新

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
-from routers import user, items #ここにリソースを追加
+from routers import user, items, purchase #ここにリソースを追加
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(items.router)
+app.include_router(purchase.router)  # アイテム関連のルーターを追加
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://hackathon-frontend-jzjx9dx7n-haruhisaasadas-projects.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://hackathon-frontend-f1bve0ub4-haruhisaasadas-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
