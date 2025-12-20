44c4c2e2dd84a1aa4db51db351f8c06779992eab
HaruhisaAsada <uttdjasd12@g.ecc.u-tokyo.ac.jp>
2025-12-20 14:33:16 +0900

    CORSミドルウェアの許可されたオリジンを更新しました。

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://hackathon-frontend-ecq0ty9fs-haruhisaasadas-projects.vercel.app",
+        "https://hackathon-frontend-l6isbymy5-haruhisaasadas-projects.vercel.app",
     ],
     allow_origin_regex=r"https://hackathon-frontend-.*\.vercel\.app",
     allow_credentials=True,
